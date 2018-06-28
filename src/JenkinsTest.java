d8f288d21099153704753d331ced0141419dc3fc
w27015 - Sukhada <sukhada.pawar@sensis.com.au>
2018-06-28 14:04:47 +0530

    Initial Commit

---
+++ src/JenkinsTest.java
@@ -0,0 +1,10 @@
+import  java.io.*;
+
+public class JenkinsTest {
+
+
+    public static void main(String args[]){
+
+        System.out.println("This is test project for Jenkins job to run");
+    }
+}
